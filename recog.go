c3e441f15dbb94e908aea7070599bda5617a0f7f
Comdex <wcomdex@foxmail.com>
2015-06-12 10:14:22 +0800

    add function binaryzation

--- recog.go
+++ recog.go
@@ -4,6 +4,7 @@ import(
 	"math"
 )
 
+//calculate Cosine Similarity of two images, input two file path
 func CosineSimilarity(src1 string, src2 string)(cossimi float64,err error){
 	matrix1,err1:=ResizeForMatrix(src1,80,60)
 	if err1 != nil {
@@ -24,5 +25,31 @@ func CosineSimilarity(src1 string, src2 string)(cossimi float64,err error){
 	cos22:=math.Sqrt(Dot(myy,myy))
 	
 	cossimi = cos1/(cos21*cos22)
+	return
+}
+
+//binaryzation process of image matrix , threshold can use 127 to test
+func Binaryzation(src [][][]uint8, threshold int)(imgMatrix [][][]uint8, err error) {
+	imgMatrix,err = RGB2Gray(src)
+	if err != nil {
+		return
+	}
+	
+	height:=len(imgMatrix)
+	width:=len(imgMatrix[0])
+	for i:=0; i<height; i++ {
+		for j:=0; j<width; j++ {
+			var rgb int = int(imgMatrix[i][j][0])+int(imgMatrix[i][j][1])+int(imgMatrix[i][j][2])
+			if rgb > threshold {
+				rgb = 255
+			}else{
+				rgb = 0
+			}
+			imgMatrix[i][j][0]=uint8(rgb)
+			imgMatrix[i][j][1]=uint8(rgb)
+			imgMatrix[i][j][2]=uint8(rgb)
+		}
+	}
+	
 	return
 }
